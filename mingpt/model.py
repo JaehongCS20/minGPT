4ab6dc57015d1a8885c8d4515a6aa6c709d180e6
JaehongCS20 <jaehongcho@kaist.ac.kr>
2024-01-03 16:16:32 +0000

    Make one output (logits)

--- mingpt/model.py
+++ mingpt/model.py
@@ -277,7 +277,8 @@ class GPT(nn.Module):
         if targets is not None:
             loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
 
-        return logits, loss
+        # return logits, loss
+        return logits
 
     @torch.no_grad()
     def generate(self, idx, max_new_tokens, temperature=1.0, do_sample=False, top_k=None):
